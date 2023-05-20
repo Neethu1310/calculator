25c56d598e5e253035b9c65d48afc591326617d2
Neethu1310 <bindujayan20@gmail.com>
2023-05-21 01:12:31 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,31 @@
+// display
+function display(num){
+    output.value += num;  //id.value=to get value
+
+}
+
+// clear =AC
+
+function clearAll(){
+    output.value="";
+}
+
+// Evaluvate
+
+function evaluvateExp(){
+    // method-1
+    // exp=output.value;  //exp=7+7
+    // result=eval(exp); //result=14
+    // output.value=result; //display=14
+
+    // method-2
+    output.value=eval(output.value);
+}
+
+
+// remove last item
+
+function removeLast(){
+    currentExp=output.value;
+    output.value=currentExp.slice(0,-1);
+}
